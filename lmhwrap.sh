ea5eb2e6aab2123fec9754add2b5f7257ed57ce8
Tom Wiesing <tkw01536@gmail.com>
2015-02-05 17:51:59 +0100

    Updated data directory

--- lmhwrap.sh
+++ lmhwrap.sh
@@ -4,6 +4,11 @@ lmh_repo="tkw01536/localmh"
 lmh_configfile="$HOME/.lmh_docker"
 lmh_mountdir="$(pwd)"
 
+echo $LMH_CONTENT_DIR
+if [ -d "$LMH_CONTENT_DIR" ]; then
+  lmh_mountdir="$LMH_CONTENT_DIR"
+fi
+
 # Executables needed
 docker=$(which docker)
 
@@ -35,11 +40,22 @@ if [ "$1" == "help" ]; then
 
 Usage: $0 [start|status|stop|destroy|help]
 
-  start   Connects to a container for lmh. Default.
+  start   Connects to a container for lmh. Creates a new container if it does
+          not already exist. Default.
   status  Checks the status of the container.
   stop    Stops the container for lmh.
   destroy Destroys the local lmh container.
   help    Displays this help message.
+
+When a new container is created the MathHub directory in the lmh instance will
+have to be mounted. By default, this script mounts the current directory. This
+behaviour can be overriden by setting the LMH_CONTENT_DIR environment variable
+to an existing directory.
+Example: 
+  LMH_CONTENT_DIR="\$HOME/localmh/MathHub" $0 start
+    If no container exists, creates a new one with the directory
+    \$HOME/localmh/MathHub used as a directory for data files. If a container
+    already exists, attaches to it.
 """
   exit 0
 fi
@@ -53,20 +69,29 @@ if [ "$1" == "stop" ]; then
 fi
 
 #
+# For the destroy command remove the container
+#
+if [ "$1" == "destroy" ]; then
+  docker rm -f $docker_pid
+  exit $?
+fi
+
+#
 # For the status command, show the status
 #
 
 if [ "$1" == "status" ]; then
   if [ "$docker_pid" == "" ]; then
-    echo "Container Id: <none>"
+    echo "Container Id:   <none>"
   else
-    echo "Container Id: $docker_pid"
+    echo "Container Id:   $docker_pid"
     if [ "$(docker inspect --format='{{ .State.Running }}' $docker_pid)" == "false" ]; then
-      echo "Status:       Not running"
+      echo "Status:         Not running"
     else
-      echo "Status:       Running"
+      echo "Status:         Running"
     fi
   fi
+  echo "Data directory: $lmh_mountdir"
   exit 0
 fi
 
@@ -74,18 +99,19 @@ fi
 # Unknown command
 #
 if [ "$1" != "start" ]; then
-  if [  "$1" != "" ]; then
+  if [ "$1" != "" ]; then
     echo "Unknown command. "
     exit 1
   fi
 fi
+
 
 #
 # Create container if it does not exist.
 #
 if [ "$(docker inspect --format='{{ .State.Running }}' $docker_pid 2> /dev/null || echo 'no')" == "no" ]; then
   echo "Creating new container for lmh ..."
-  docker_pid=$(docker create $lmh_repo)
+  docker_pid=$(docker create $lmh_repo -v "$lmh_mountdir:/lmh_content_dir")
   echo $docker_pid > "$lmh_configfile"
 fi
 
@@ -97,5 +123,5 @@ if [ "$(docker inspect --format='{{ .State.Running }}' $docker_pid)" == "false" 
 fi
 
 # and connect to it.
-docker exec -t -i $docker_pid /bin/bash
+docker exec -t -i $docker_pid /bin/sh -c 'cd $HOME/localmh; /bin/bash'
 exit $?
